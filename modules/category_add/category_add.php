3b1643d6cf011c69889ed91d8c98102037a3eb7d
Carlos Santos <geral@nexus-pt.eu>
2014-05-20 20:56:25 +0100

    Update category_add.php

--- modules/category_add/category_add.php
+++ modules/category_add/category_add.php
@@ -8,36 +8,48 @@
         <h2>Lingua 1</h2>
 		<span id="label">Categoria</span>
 		<input type="text" name="title_1"/>
+		<span id="label">Descrição</span>
+		<?php print returnEditor('content_1',null); ?>
         <div class="separator30"></div> 
     </div>
     <div <?php if (!$configuration['lang_2_state']) {echo 'style="display: none;"';} ?>>
         <h2>Lingua 2</h2>
     	<span id="label">Categoria</span>
 		<input type="text" name="title_2"/>
+		<span id="label">Descrição</span>
+		<?php print returnEditor('content_2',null); ?>
         <div class="separator30"></div>
     </div>
     <div <?php if (!$configuration['lang_3_state']) {echo 'style="display: none;"';} ?>>
         <h2>Lingua 3</h2>
     	<span id="label">Categoria</span>
 		<input type="text" name="title_3"/>
+		<span id="label">Descrição</span>
+		<?php print returnEditor('content_3',null); ?>
         <div class="separator30"></div>
     </div>
     <div <?php if (!$configuration['lang_4_state']) {echo 'style="display: none;"';} ?>>
         <h2>Lingua 4</h2>
     	<span id="label">Categoria</span>
 		<input type="text" name="title_4"/>
+		<span id="label">Descrição</span>
+		<?php print returnEditor('content_4',null); ?>
         <div class="separator30"></div>
     </div>
     <div <?php if (!$configuration['lang_5_state']) {echo 'style="display: none;"';} ?>>
         <h2>Lingua 5</h2>
     	<span id="label">Categoria</span>
 		<input type="text" name="title_5"/>
+		<span id="label">Descrição</span>
+		<?php print returnEditor('content_5',null); ?>
         <div class="separator30"></div>
     </div>
     <div <?php if (!$configuration['lang_6_state']) {echo 'style="display: none;"';} ?>>
         <h2>Lingua 6</h2>
     	<span id="label">Categoria</span>
 		<input type="text" name="title_6"/>
+		<span id="label">Descrição</span>
+		<?php print returnEditor('content_6',null); ?>
         <div class="separator30"></div>
     </div>
     <div>
@@ -50,11 +62,6 @@
             }    
         ?>
         </select>
-        <div class="separator30"></div>
-    </div>
-    <div>
-        <span id="label">Descrição</span>
-		<?php print returnEditor('description',null); ?>
         <div class="separator30"></div>
     </div>
     <div>
@@ -80,16 +87,26 @@
         $object_category = new category();
         $object_category->setContent(
             $_REQUEST['title_1'],
+            $_REQUEST['content_1']
             $_REQUEST['title_2'],
+            $_REQUEST['content_2']
             $_REQUEST['title_3'],
+            $_REQUEST['content_3']
             $_REQUEST['title_4'],
+            $_REQUEST['content_4']
             $_REQUEST['title_5'],
+            $_REQUEST['content_5']
             $_REQUEST['title_6'],
+            $_REQUEST['content_6']
             $_REQUEST['code'],
-            $_REQUEST['description']
             );
+            
+        $object_category->setUserId();
+        $object_category->setDate();
+        $object_category->setDateUpdate();
         $object_category->setPublished($_REQUEST['published']);
         $object_category->setCategoryType($_REQUEST['section']);
+        
         if ($object_category->insert()) {
             print 'sucess';
         } else {
