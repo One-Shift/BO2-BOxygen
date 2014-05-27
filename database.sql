0cc9987d912167daacf222b9fc4ffd95c3169ea5
Carlos Santos <geral@nexus-pt.eu>
2014-05-27 17:16:32 +0100

    Update database.sql
    adding service column on products
    define if a product was a product (false) or service (true)

--- database.sql
+++ database.sql
@@ -3,36 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: May 20, 2014 at 11:23 
+-- Generation Time: May 27, 2014 at 06:14 
 -- Server version: 5.5.36
 -- PHP Version: 5.4.25
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `test`
---
-
-DELIMITER $$
---
--- Procedures
---
-CREATE DEFINER=`root`@`localhost` PROCEDURE `test_multi_sets`()
-    DETERMINISTIC
-begin
-        select user() as first_col;
-        select user() as first_col, now() as second_col;
-        select user() as first_col, now() as second_col, now() as third_col;
-        end$$
-
-DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -239,6 +215,7 @@ CREATE TABLE IF NOT EXISTS `prefix_products` (
   `title_6` varchar(255) DEFAULT NULL,
   `content_6` text,
   `code` text,
+  `service` tinyint(1) NOT NULL DEFAULT '0',
   `price` double NOT NULL DEFAULT '0',
   `vat` double NOT NULL DEFAULT '0',
   `discount` double NOT NULL DEFAULT '0',
@@ -256,9 +233,9 @@ CREATE TABLE IF NOT EXISTS `prefix_products` (
 -- Dumping data for table `prefix_products`
 --
 
-INSERT INTO `prefix_products` (`id`, `reference`, `title_1`, `content_1`, `title_2`, `content_2`, `title_3`, `content_3`, `title_4`, `content_4`, `title_5`, `content_5`, `title_6`, `content_6`, `code`, `price`, `vat`, `discount`, `user_id`, `category_id`, `date`, `date_update`, `published`, `onhome`, `priority`) VALUES
-(5, '', 'aqui referencia', 'produto de teste', 'alguma descrição<br>', '', '<br>', '', '<br>', '', '<br>', '', '<br>', '', '<br>', 1, 23, 0, 3, 0, '2014-05-10 19:12:56', '0000-00-00 00:00:00', 1, 0, NULL),
-(6, '', 'nome', 'aqui texto<br>', 'nome', '<br>', 'nome', '<br>', 'nome', '<br>', 'nome', '<br>', 'nome', '<br>', '', 1.23, 23.25, 0, 3, 2, '2014-05-18 14:53:40', '2014-05-18 14:53:40', 0, 0, NULL);
+INSERT INTO `prefix_products` (`id`, `reference`, `title_1`, `content_1`, `title_2`, `content_2`, `title_3`, `content_3`, `title_4`, `content_4`, `title_5`, `content_5`, `title_6`, `content_6`, `code`, `service`, `price`, `vat`, `discount`, `user_id`, `category_id`, `date`, `date_update`, `published`, `onhome`, `priority`) VALUES
+(5, '', 'aqui referencia', 'produto de teste', 'alguma descrição<br>', '', '<br>', '', '<br>', '', '<br>', '', '<br>', '', '<br>', 0, 1, 23, 0, 3, 0, '2014-05-10 19:12:56', '0000-00-00 00:00:00', 1, 0, NULL),
+(6, '', 'nome', 'aqui texto<br>', 'nome', '<br>', 'nome', '<br>', 'nome', '<br>', 'nome', '<br>', 'nome', '<br>', '', 0, 1.23, 23.25, 0, 3, 2, '2014-05-18 14:53:40', '2014-05-18 14:53:40', 0, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -297,7 +274,3 @@ CREATE TABLE IF NOT EXISTS `prefix_variables` (
   `value` varchar(50) CHARACTER SET utf8 NOT NULL,
   UNIQUE KEY `variable` (`variable`)
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
