945ccbe62399a960ec17cb9790796ea561a597f9
Carlos Santos <geral@nexus-pt.eu>
2014-06-01 15:43:57 +0100

    correct syntax

--- modules/user_add/user_add.php
+++ modules/user_add/user_add.php
@@ -1,23 +1,22 @@
 <h1><?php echo $language["mod-user-add-title"]; ?></h1>
     <?php
-    if (!isset($_REQUEST['save'])) { 
+    if (!isset($_POST['save'])) { 
     ?>
     <form method="post">
         <span id="label">Rank</span>
     	<select name="rank">
             <option value="null">Selecione um rank de utilizador</option>
-            <option value="owner">Owner</option>
             <option value="manager">Mananger</option>
             <option value="member">Member</option>
         </select>
     	<span id="label">Nome</span>
-        <input type="text" name="username"/>
+        <input type="text" name="username" />
         <span id="label">E-Mail</span>
-        <input type="email" name="email"/>
+        <input type="email" name="email" />
         <span id="label">Password</span>
-        <input type="password" name="password"/>	
+        <input type="password" name="password" />	
         <span id="label">Confirme a password</span>
-        <input type="password" name="confirm_password"/>
+        <input type="password" name="confirm_password" />
         
         <div class="bottom-area">  
               <button type="submit" name="save" onclick="if ($('input[name=password]').val() == $('input[name=confirm_password]').val() && $('input[name=password]').val() != '' && $('input[name=confirm_password]').val() != '') {return true;} else {alert('Passowrds não coincidem!'); return false;}"><?php echo $language['save']; ?></button>
@@ -26,12 +25,12 @@
     </form>    
     <?php
     }else{
-        if(filter_var($_REQUEST['email'], FILTER_VALIDATE_EMAIL)){        
+        if(filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){        
             $user = new user();
-            $user->setUsername($_REQUEST['username']);
-            $user->setPassword($_REQUEST['password']);
-            $user->setEmail($_REQUEST['email']);
-            $user->setRank($_REQUEST['rank']);
+            $user->setUsername($_POST['username']);
+            $user->setPassword($_POST['password']);
+            $user->setEmail($_POST['email']);
+            $user->setRank($_POST['rank']);
             
             if($user->existUserByName() == 0){
                 if($user->insert()){
