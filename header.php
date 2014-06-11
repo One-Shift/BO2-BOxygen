c3589185f8d74967e43ae1afccafdc8a49078b51
Carlos Santos <geral@nexus-pt.eu>
2014-06-11 17:48:29 +0100

    Update header.php

--- header.php
+++ header.php
@@ -4,11 +4,11 @@
 	include './connect.php';
 
 	// CLASSES
-	include './class/class_article.php';
-	include './class/class_product.php';
-	include './class/class_category.php';
-	include './class/class_user.php';
-	include './class/class_newsletters.php';
+	include './class/class.article.php';
+	include './class/class.product.php';
+	include './class/class.category.php';
+	include './class/class.user.php';
+	include './class/class.newsletters.php';
 
 	// OUTROS
 	include './functions.php';
