7eeccfa2d25709f50349eed6696b0cc82e74f0b8
Carlos Santos <geral@nexus-pt.eu>
2014-06-25 19:56:15 +0100

    Update upload_script_img.php

--- modules/upload/upload_script_img.php
+++ modules/upload/upload_script_img.php
@@ -8,8 +8,8 @@
 <html>
     <head>
         <title>IMG Uploader</title>
-        <script type="text/javascript" src="./../../site-assets/jquery.js"></script>
-        <script type="text/javascript" src="./../../site-assets/script.js"></script>
+        <script type="text/javascript" src="./../../site-assets/js/jquery.js"></script>
+        <script type="text/javascript" src="./../../site-assets/js/script.js"></script>
         <style type="text/css">
             * {
                 font-family: Sans-Serif;
