375197290ff204cde7686ea679d732d6263803c9
Carlos Santos <geral@nexus-pt.eu>
2014-07-05 19:51:19 +0100

    include class.GibberishAES.php

--- header.php
+++ header.php
@@ -1,15 +1,16 @@
 <?php
-	// CONFIGURAÇÕES
-	include './configuration.php';
-	include './connect.php';
+    // CONFIGURAÇÕES
+    include './configuration.php';
+    include './connect.php';
 
-	// CLASSES
-	include './class/class.article.php';
-	include './class/class.product.php';
-	include './class/class.category.php';
-	include './class/class.user.php';
-	include './class/class.newsletters.php';
+    // CLASSES
+    include "./class/class.GibberishAES.php";
+    include './class/class.article.php';
+    include './class/class.product.php';
+    include './class/class.category.php';
+    include './class/class.user.php';
+    include './class/class.newsletters.php';
 
-	// OUTROS
-	include './functions.php';
-	include './languages/'.$configuration['language'].'.php';
+    // OUTROS
+    include './functions.php';
+    include './languages/'.$configuration['language'].'.php';
