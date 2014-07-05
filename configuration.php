3e5085f5dfca35806e659740b12027e99344a7ec
Carlos Santos <geral@nexus-pt.eu>
2014-07-05 19:49:48 +0100

    remove spaces and tabs

--- configuration.php
+++ configuration.php
@@ -1,62 +1,61 @@
 <?php
-    // system   wr8wruwE
     // demo demo
 	$configuration = array(
         //Site info
-        'site-name'             => ' :: versão demo :: ',
-        'site-slogan'           => '',
-        'site-owner'            => 'Carlos Santos',
+        'site-name' => ' :: versão demo :: ',
+        'site-slogan' => '',
+        'site-owner' => 'Carlos Santos',
         
         //front-end info
-        "per-page"              => 30,
-        "nr-links"              => 4,
+        "per-page" => 30,
+        "nr-links" => 4,
         "path" => "Path-here ex.: /new or /old",
 		
         //back-end info
-        'BO2-version'		=> '1.1.0',
-        'BO2-subversion'	=> 'b',
-        'BO2-name'			=> 'BOxygen :: BO²',
-        'language'			=> 'en',
+        'BO2-version' => '1.1.0',
+        'BO2-subversion' => 'b',
+        'BO2-name' => 'BOxygen :: BO²',
+        'language' => 'en',
         "secret-key" => "GJTBpKregE9WgXc",
         
         //MySql configuration
-        'mysql-user'		=> 'username',
-        'mysql-password'	=> 'password',
-        'mysql-host'		=> '127.0.0.1',
-        'mysql-database'	=> 'database',
-        'mysql-prefix'		 => 'prefix',
+        'mysql-user' => 'username',
+        'mysql-password' => 'password',
+        'mysql-host' => '127.0.0.1',
+        'mysql-database' => 'database',
+        'mysql-prefix' => 'prefix',
         
         //Cookie info 
-        'cookie'			=> 'cookie',
-        'cookie-time'		=> 320,
+        'cookie' => 'cookie',
+        'cookie-time' => 320,
         
         //contact info
-        'email'					=> 'geral@nexus-pt.eu',
+        'email' => 'geral@nexus-pt.eu',
         
         //Language conf
-        'lang_1_state'          => true,
-        'lang_1_acronym'        => "pt",
-        'lang_1_name'           => "portuguese",
-        'lang_2_state'          => true,
-        'lang_2_acronym'        => "en",
-        'lang_2_name'           => "english",
-        'lang_3_state'          => true,
-        'lang_3_acronym'        => "es",
-        'lang_3_name'           => "spanish",
-        'lang_4_state'          => true,
-        'lang_4_acronym'        => "fr",
-        'lang_4_name'           => "french",
-        'lang_5_state'          => true,
-        'lang_5_acronym'        => "5",
-        'lang_5_name'           => "sample_5",
-        'lang_6_state'          => true,
-        'lang_6_acronym'        => "6",
-        'lang_6_name'           => "sample_6",
-		'store'					=> true,	
+        'lang_1_state' => true,
+        'lang_1_acronym' => "pt",
+        'lang_1_name' => "portuguese",
+        'lang_2_state' => true,
+        'lang_2_acronym' => "en",
+        'lang_2_name' => "english",
+        'lang_3_state' => true,
+        'lang_3_acronym' => "es",
+        'lang_3_name' => "spanish",
+        'lang_4_state' => true,
+        'lang_4_acronym' => "fr",
+        'lang_4_name' => "french",
+        'lang_5_state' => true,
+        'lang_5_acronym' => "5",
+        'lang_5_name' => "sample_5",
+        'lang_6_state' => true,
+        'lang_6_acronym' => "6",
+        'lang_6_name' => "sample_6",
+	'store' => true,	
         
         //analytics
-        'analytics'        => "",
+        'analytics' => "",
         
-       'category_sections'      => array("products","articles","user"),
+       'category_sections' => array("products","articles","user"),
         
 	);
