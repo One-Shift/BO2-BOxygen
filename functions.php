50be5048404aa7448fcd7b36e6ed67585c360d81
Carlos Santos <you@example.com>
2014-07-11 17:14:00 +0100

    remove returnImageExt function, no need

--- functions.php
+++ functions.php
@@ -24,26 +24,6 @@
         ."<script type=\"text/javascript\">new nicEditor({fullPanel : true}).panelInstance('%s');</script>",
         $textareanema, $textareanema, $content, $textareanema);
     }
-
-    function returnImageExt($filetype) {
-        switch ($filetype) {
-            case "image/jpg":
-                    return "jpg";
-                    break;
-            case "image/jpeg":
-                    return "jpg";
-                    break;
-            case "image/png":
-                    return "png";
-                    break;
-            case "image/bmp":
-                    return "bmp";
-                    break;
-            default:
-                    return null;
-                    break;
-        }
-    }
   
     function sendEmail ($from, $to, $subject, $message) {
         $headers = 'From: '.$from."\r\n" .
@@ -95,33 +75,32 @@
     }
 
     
-/**
- * Get either a Gravatar URL or complete image tag for a specified email address.
- *
- * @param string $email The email address
- * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
- * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
- * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
- * @param boole $img True to return a complete IMG tag False for just the URL
- * @param array $atts Optional, additional key/value attributes to include in the IMG tag
- * @return String containing either just a URL or a complete image tag
- * @source http://gravatar.com/site/implement/images/php/
- */
-function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'x', $img = false, $atts = array() ) {
-	
-	$url = 'http://www.gravatar.com/avatar/';
-	
-	$url .= md5( strtolower( trim( $email ) ) );
-	
-	$url .= "?s=$s&d=$d&r=$r";
-	
-	if ( $img ) {
-		$url = '<img src="' . $url . '"';
-		foreach ( $atts as $key => $val )
-			$url .= ' ' . $key . '="' . $val . '"';
-		$url .= ' />';
-	}
-	
-	return $url;
-}
-?>
+    /**
+     * Get either a Gravatar URL or complete image tag for a specified email address.
+     *
+     * @param string $email The email address
+     * @param string $s Size in pixels, defaults to 80px [ 1 - 2048 ]
+     * @param string $d Default imageset to use [ 404 | mm | identicon | monsterid | wavatar ]
+     * @param string $r Maximum rating (inclusive) [ g | pg | r | x ]
+     * @param boole $img True to return a complete IMG tag False for just the URL
+     * @param array $atts Optional, additional key/value attributes to include in the IMG tag
+     * @return String containing either just a URL or a complete image tag
+     * @source http://gravatar.com/site/implement/images/php/
+     */
+    function get_gravatar( $email, $s = 80, $d = 'mm', $r = 'x', $img = false, $atts = array() ) {
+
+        $url = 'http://www.gravatar.com/avatar/';
+
+        $url .= md5( strtolower( trim( $email ) ) );
+
+        $url .= "?s=$s&d=$d&r=$r";
+
+        if ( $img ) {
+            $url = '<img src="' . $url . '"';
+            foreach ( $atts as $key => $val )
+                $url .= ' ' . $key . '="' . $val . '"';
+            $url .= ' />';
+        }
+
+        return $url;
+    }
