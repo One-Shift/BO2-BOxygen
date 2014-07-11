6c9c0dca28077fe988120bbb39be950c7979afd5
Carlos Santos <you@example.com>
2014-07-11 17:20:35 +0100

    add some corrections

--- languages/en.php
+++ languages/en.php
@@ -3,78 +3,78 @@
         "system" => array(),
 
         "template"  => array(
-            "add"   => "Adicionar",
-            "edit"  => "Editar",
-            "del"   => "Eliminar",
-            "enable"   => "Ativar",
-            "disable"   => "Desactivar",
+            "add"   => "Add",
+            "edit"  => "Edit",
+            "del"   => "Del",
+            "enable"   => "Enable",
+            "disable"   => "Disable",
         ),
 
         "actions" => array(
-            "sucess" => "",
-            "failure" => "",
-            "error" => "",
+            "sucess" => "All functions were executed successfully.",
+            "failure" => "Something went wrong. Try again, if the error persists contact your webmaster and explain the situation well.",
+            "error" => "An internal error occurred. Talk to your webmaster and explain the situation well.",
         ),
 
         "article"   => array(),
 
         // general
-        'confirm'                   => 'Are you sure?',
-        'save'                      => 'Save',
-        'cancel'                    => 'Cancel',
+        "confirm" => "Are you sure?",
+        "save" => "Save",
+        "cancel" => "Cancel",
 
 		// login
-		'login-b-login'	            =>	'Login',
-		'login-b-reset'	            =>	'Reset',
-		'login-sucess'  	        =>	'Your session has been initiated!',
-		'login-error'	            =>	'Error! Something are wrong!',
+		"login-b-login" =>	"Login",
+		"login-b-reset" =>	"Reset",
+		"login-sucess" =>	"Your session has been initiated!",
+		"login-error" =>	"Error! Something are wrong!",
 
 		//backoffice
-        'mysql-connection-error'	=>	'Error! Cannot connect to database!',
-        'mysql-connection-sucess'	=>	'Your connection to the database has been successfully',
+        "mysql-connection-error" =>	"Error! Cannot connect to database!",
+        "mysql-connection-sucess" =>	"Your connection to the database has been successfully",
 
         // other
-        'menu'			            => 'Menu',
-		'back'			            => 'Back',
+        "menu" => "Menu",
+		"back" => "Back",
 
 		// menu
-		'menu-begin' 	           => 'Home',
-		'menu-logout' 	          => 'Logout',
+		"menu-begin" => "Home",
+		"menu-logout" => "Logout",
 
-		'menu-articles'             => 'Articles',
-        'menu-products'             => 'Products',
-		'menu-newsletters'		  => 'Newsletters',
-        'menu-categories'           => 'Categories',
-        'menu-users'                => 'Users',
-		'menu-account'              => 'Account',
-		'menu-develop'              => 'Developing',
+		"menu-articles" => "Articles",
+        "menu-products" => "Products",
+		"menu-newsletters" => "Newsletters",
+        "menu-categories" => "Categories",
+        "menu-users" => "Users",
+		"menu-account" => "Account",
+		"menu-develop" => "Developing",
 
 
         // home
-		'home'		            	=> 'About BO2',
+		"home" => "About BO2",
 
 		// mod_news
-		'mod-news-add-title'		=> 'Add News',
-		'mod-news-add-fields-empty'	=> 'Some fields are Required',
+		"mod-news-add-title" => "Add News",
+		"mod-news-add-fields-empty"	=> "Some fields are Required",
 
         // mod_category
-        'mod-category-add-title'    => 'Add Category',
-        'mod-category-edit-title'   => 'Edit Category',
-        'articles'                  => 'Articles',
-        'products'                    => 'Products',
-        'user'                      => 'User',
+        "mod-category-add-title" => "Add Category",
+        "mod-category-edit-title" => "Edit Category",
+        "articles" => "Articles",
+        "products" => "Products",
+        "user" => "User",
 
         //mod_products
-        'mod-product-add-title'     => 'Add Products',
-        'mod-product-edit-title'    => 'Edit Product',
+        "mod-product-add-title" => "Add Products",
+        "mod-product-edit-title" => "Edit Product",
 
 	    //mod_user
-        'mod-user-add-title'        => 'Add User',
-        'mod-user-edit-title'       => 'Edit User',
+        "mod-user-add-title" => "Add User",
+        "mod-user-edit-title" => "Edit User",
 
         // suport
-        'suport-subject'            => 'Support',
+        "suport-subject" => "Support",
 
 		//404
-		'not_found'		            => 'The page you are search do not exist!',
+		"not_found" => "The page you are search do not exist!",
 	);
