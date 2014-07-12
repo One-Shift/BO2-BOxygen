e1f71c45e4e966c9bdb0302d04c7c2f3e9700caf
Carlos Santos <carlos@nexus-pt.eu>
2014-07-12 20:02:15 +0100

    order classes

--- header.php
+++ header.php
@@ -4,12 +4,16 @@
     include './connect.php';
 
     // CLASSES
+    include "./class/class.article.php";
+    include "./class/class.cart.php";
+    include "./class/class.category.php";
     include "./class/class.GibberishAES.php";
-    include './class/class.article.php';
-    include './class/class.product.php';
-    include './class/class.category.php';
-    include './class/class.user.php';
-    include './class/class.newsletters.php';
+    include "./class/class.history.php";
+    include "./class/class.newsletters.php";
+    include "./class/class.orders.php";
+    include "./class/class.product.php";
+    include "./class/class.productsearch.php";
+    include "./class/class.user.php";
 
     // OUTROS
     include './functions.php';
