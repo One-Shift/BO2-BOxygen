a6bdf9705c2d3f108cf2ebc6d11e1b0587b23d8b
Carlos Santos <carlos@nexus-pt.eu>
2014-07-13 16:15:34 +0100

    align text

--- configuration.php
+++ configuration.php
@@ -1,61 +1,59 @@
 <?php
-    // demo demo
-    $configuration = array(
-       //Site info
-       "site-name" => " :: versão demo :: ",
-       "site-slogan" => "",
-       "site-owner" => "Carlos Santos",
+	$configuration = array(
+		//Site info
+		"site-name" => " :: versão demo :: ",
+		"site-slogan" => "",
+		"site-owner" => "Carlos Santos",
 
-       //front-end info
-       "per-page" => 30,
-       "nr-links" => 4,
-       "path" => "Path-here ex.: /new or /old",
-		
-       //back-end info
-       "BO2-version" => "1.1.0",
-       "BO2-subversion" => "b",
-       "BO2-name" => "BOxygen :: BO²",
-       "language" => "en",
-       "secret-key" => "GJTBpKregE9WgXc",
+		//front-end info
+		"per-page" => 30,
+		"nr-links" => 4,
+		"path" => "Path-here ex.: /new or /old",
 
-       //MySql configuration
-       "mysql-user" => "username",
-       "mysql-password" => "password",
-       "mysql-host" => "127.0.0.1",
-       "mysql-database" => "database",
-       "mysql-prefix" => "prefix",
+		//back-end info
+		"BO2-version" => "1.1.0",
+		"BO2-subversion" => "b",
+		"BO2-name" => "BOxygen :: BO²",
+		"language" => "en",
+		"secret-key" => "GJTBpKregE9WgXc",
 
-       //Cookie info
-       "cookie" => "cookie",
-       "cookie-time" => 320,
+		//MySql configuration
+		"mysql-user" => "username",
+		"mysql-password" => "password",
+		"mysql-host" => "127.0.0.1",
+		"mysql-database" => "database",
+		"mysql-prefix" => "prefix",
 
-       //contact info
-       "email" => "geral@nexus-pt.eu",
+		//Cookie info
+		"cookie" => "cookie",
+		"cookie-time" => 320,
 
-       //Language conf
-       "lang_1_state" => true,
-       "lang_1_acronym" => "pt",
-       "lang_1_name" => "portuguese",
-       "lang_2_state" => true,
-       "lang_2_acronym" => "en",
-       "lang_2_name" => "english",
-       "lang_3_state" => true,
-       "lang_3_acronym" => "es",
-       "lang_3_name" => "spanish",
-       "lang_4_state" => true,
-       "lang_4_acronym" => "fr",
-       "lang_4_name" => "french",
-       "lang_5_state" => true,
-       "lang_5_acronym" => "5",
-       "lang_5_name" => "sample_5",
-       "lang_6_state" => true,
-       "lang_6_acronym" => "6",
-       "lang_6_name" => "sample_6",
-	   "store" => true,
-        
-        //analytics
-        "analytics" => "",
-        
-       "category_sections" => array("products","articles","user"),
-        
+		//contact info
+		"email" => "geral@nexus-pt.eu",
+
+		//Language conf
+		"lang_1_state" => true,
+		"lang_1_acronym" => "pt",
+		"lang_1_name" => "portuguese",
+		"lang_2_state" => true,
+		"lang_2_acronym" => "en",
+		"lang_2_name" => "english",
+		"lang_3_state" => true,
+		"lang_3_acronym" => "es",
+		"lang_3_name" => "spanish",
+		"lang_4_state" => true,
+		"lang_4_acronym" => "fr",
+		"lang_4_name" => "french",
+		"lang_5_state" => true,
+		"lang_5_acronym" => "5",
+		"lang_5_name" => "sample_5",
+		"lang_6_state" => true,
+		"lang_6_acronym" => "6",
+		"lang_6_name" => "sample_6",
+		"store" => true,
+
+		//analytics
+		"analytics" => "",
+
+		"category_sections" => array("products","articles","user"),
 	);
