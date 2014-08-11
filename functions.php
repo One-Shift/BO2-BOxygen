e09432d87e4d964f2313899d4e49ada46302c4ee
Carlos Santos <geral@nexus-pt.eu>
2014-08-11 19:13:48 +0100

    add clean function

--- functions.php
+++ functions.php
@@ -95,6 +95,11 @@
         return $randomString;
     }
 
+	function clean ($string) {
+	   $string = str_replace(' ', '-', $string); // Replaces all spaces with hyphens.
+	   return preg_replace('/[^A-Za-z0-9\-]/', '', $string); // Removes special chars.
+	}
+
     
     /**
      * Get either a Gravatar URL or complete image tag for a specified email address.
