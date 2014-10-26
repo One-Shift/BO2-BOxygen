1a9769402794058ccd010417e61f2a39875b9eae
Carlos Santos <geral@nexus-pt.eu>
2014-10-26 02:55:56 +0000

    display error of mysqli connection

--- connect.php
+++ connect.php
@@ -1,3 +1,9 @@
 <?php
-	$mysqli = mysqli_connect($configuration["mysql-host"], $configuration["mysql-user"], $configuration["mysql-password"], $configuration["mysql-database"]) or die("Errormessage: %s\n".$mysqli->error);
+	$mysqli = mysqli_connect($configuration["mysql-host"], $configuration["mysql-user"], $configuration["mysql-password"], $configuration["mysql-database"]);
+	
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+	
 	$mysqli->set_charset("utf8");
