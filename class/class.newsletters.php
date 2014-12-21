f7ac6ead4bbd1c8046ecc49e31842dbbce4fe685
Carlos Santos <geral@nexus-pt.eu>
2014-12-21 00:48:05 +0000

    Update class.newsletters.php

--- class/class.newsletters.php
+++ class/class.newsletters.php
@@ -1,141 +1,140 @@
 <?php
-    class newsletters {
-        protected $id;
-        protected $email;
-        protected $code;
-        protected $date;
-        protected $date_update;
-        protected $state;
 
+class newsletters {
 
-        public function __construct () {}
+	protected $id;
+	protected $email;
+	protected $code;
+	protected $date;
+	protected $date_update;
+	protected $state;
 
-        public function setId ($i) {
-            $this->id = $i;
-        }
+	public function __construct() {
+		
+	}
 
-        public function setEmail ($e) {
-            $this->email = $e;
-        }
+	public function setId($i) {
+		$this->id = $i;
+	}
 
-        public function setCode ($c) {
-            $this->code = sha1(md5(sha1(md5(time()."_".$c))));
-        }
+	public function setEmail($e) {
+		$this->email = $e;
+	}
 
-        public function setDate() {
-            $this->date = date('Y-m-d H:i:s',time());
-        }
+	public function setCode($c) {
+		$this->code = sha1(md5(sha1(md5(time() . "_" . $c))));
+	}
 
-        public function setDateUpdate() {
-            $this->date_update = date('Y-m-d H:i:s',time());
-        }
+	public function setDate() {
+		$this->date = date('Y-m-d H:i:s', time());
+	}
 
-        public function insert() {
-            global $configuration;
-            global $mysqli;
+	public function setDateUpdate() {
+		$this->date_update = date('Y-m-d H:i:s', time());
+	}
 
-            $query = sprintf("INSERT INTO %s_newsletters (email, code, date) VALUES ('%s','%s','%s')",
-                $configuration['mysql-prefix'], $this->email, $this->code, $this->date);
+	public function insert() {
+		global $configuration;
+		global $mysqli;
 
-            $toReturn = $mysqli->query($query);
+		$query = sprintf("INSERT INTO %s_newsletters (email, code, date) VALUES ('%s','%s','%s')", $configuration['mysql-prefix'], $this->email, $this->code, $this->date);
 
-            $this->id = $mysqli->insert_id;
+		$toReturn = $mysqli->query($query);
 
-            return $toReturn;
-        }
+		$this->id = $mysqli->insert_id;
 
-        public function update () {
-            global $configuration;
-            global $mysqli;
+		return $toReturn;
+	}
 
-            $query = sprintf("UPDATE %s_newsletters SET email = '%s', code = '%s', date_update = '%s' WHERE id = '%s'",
-                $configuration['mysql-prefix'], $this->email, $this->code, $this->date_update, $this->id);
+	public function update() {
+		global $configuration;
+		global $mysqli;
 
-            return $mysqli->query($query);
-        }
+		$query = sprintf("UPDATE %s_newsletters SET email = '%s', code = '%s', date_update = '%s' WHERE id = '%s'", $configuration['mysql-prefix'], $this->email, $this->code, $this->date_update, $this->id);
 
-        public function delete () {
-            global $configuration;
-            global $mysqli;
+		return $mysqli->query($query);
+	}
 
-            $query = sprintf("DELETE FROM %s_newsletters WHERE id = '%s'", $configuration['mysql-prefix'], $this->id);
+	public function delete() {
+		global $configuration;
+		global $mysqli;
 
-            return $mysqli->query($query);
-        }
+		$query = sprintf("DELETE FROM %s_newsletters WHERE id = '%s'", $configuration['mysql-prefix'], $this->id);
 
-        public function returnObject () {
-            return array(
-                'email' => $this->email,
-                'code' => $this->code,
-                'date' => $this->date,
-                'date_update' => $this->date_update
-            );
-        }
+		return $mysqli->query($query);
+	}
 
-        public function returnOneRegistry () {
-            global $configuration;
-            global $mysqli;
+	public function returnObject() {
+		return array(
+			'email' => $this->email,
+			'code' => $this->code,
+			'date' => $this->date,
+			'date_update' => $this->date_update
+		);
+	}
 
-            $query = sprintf("SELECT * FROM %s_newsletters WHERE id = '%s' LIMIT 1",
-                $configuration['mysql-prefix'], $this->id);
-            $source = $mysqli->query($query);
+	public function returnOneRegistry() {
+		global $configuration;
+		global $mysqli;
 
-            return $source->fetch_array(MYSQLI_ASSOC);
-        }
+		$query = sprintf("SELECT * FROM %s_newsletters WHERE id = '%s' LIMIT 1", $configuration['mysql-prefix'], $this->id);
+		$source = $mysqli->query($query);
 
-        public function existRegistryByEmail () {
-            global $configuration;
-            global $mysqli;
+		return $source->fetch_array(MYSQLI_ASSOC);
+	}
 
-            $query = sprintf("SELECT * FROM %s_newsletters WHERE email = '%s' LIMIT 1",
-                $configuration['mysql-prefix'], $this->email);
-            $source = $mysqli->query($query);
+	public function existRegistryByEmail() {
+		global $configuration;
+		global $mysqli;
 
-            return $source->num_rows;
-        }
+		$query = sprintf("SELECT * FROM %s_newsletters WHERE email = '%s' LIMIT 1", $configuration['mysql-prefix'], $this->email);
+		$source = $mysqli->query($query);
 
-        public function existRegistryByCode () {
-            global $configuration;
-            global $mysqli;
+		return $source->num_rows;
+	}
 
-            $query = sprintf("SELECT * FROM %s_newsletters WHERE code = '%s' LIMIT 1",
-                $configuration['mysql-prefix'], $this->code);
-            $source = $mysqli->query($query);
+	public function existRegistryByCode() {
+		global $configuration;
+		global $mysqli;
 
-            return $source->num_rows;
-        }
+		$query = sprintf("SELECT * FROM %s_newsletters WHERE code = '%s' LIMIT 1", $configuration['mysql-prefix'], $this->code);
+		$source = $mysqli->query($query);
 
-        public function returnAllRegistries () {
-            global $configuration;
-            global $mysqli;
+		return $source->num_rows;
+	}
 
-            $query = sprintf("SELECT * FROM %s_newsletters WHERE true", $configuration['mysql-prefix']);
-            $source = $mysqli->query($query);
+	public function returnAllRegistries() {
+		global $configuration;
+		global $mysqli;
 
-            $toReturn = array();
-            $i = 0;
+		$query = sprintf("SELECT * FROM %s_newsletters WHERE true", $configuration['mysql-prefix']);
+		$source = $mysqli->query($query);
 
-            while ($data = $source->fetch_array(MYSQLI_ASSOC)) {
-                $toReturn[$i] = $data;
-                $i++;
-            }
+		$toReturn = array();
+		$i = 0;
 
-            return $toReturn;
-        }
+		while ($data = $source->fetch_array(MYSQLI_ASSOC)) {
+			$toReturn[$i] = $data;
+			$i++;
+		}
 
-        public function enable () {
-            global $configuration;
-            global $mysqli;
+		return $toReturn;
+	}
 
-            $query = sprintf("UPDATE %s_newsletters SET active = '%s' WHERE id = '%s'", $configuration['mysql-prefix'], true, $this->id);
-            return $mysqli->query($query);
-        }
+	public function enable() {
+		global $configuration;
+		global $mysqli;
 
-        public function disable () {
-            global $configuration;
-            global $mysqli;
+		$query = sprintf("UPDATE %s_newsletters SET active = '%s' WHERE id = '%s'", $configuration['mysql-prefix'], true, $this->id);
+		return $mysqli->query($query);
+	}
 
-            $query = sprintf("UPDATE %s_newsletters SET active = '%s' WHERE id = '%s'", $configuration['mysql-prefix'], false, $this->id);
-            return $mysqli->query($query);
-        }
-    }
+	public function disable() {
+		global $configuration;
+		global $mysqli;
+
+		$query = sprintf("UPDATE %s_newsletters SET active = '%s' WHERE id = '%s'", $configuration['mysql-prefix'], false, $this->id);
+		return $mysqli->query($query);
+	}
+
+}
