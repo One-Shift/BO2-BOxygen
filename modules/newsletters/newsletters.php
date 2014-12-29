7afcb2d9ebefe982de68c91d6717a32cb8520c08
Carlos Santos <carlos@nexus-pt.eu>
2014-12-29 14:17:55 +0000

    new version

--- modules/newsletters/newsletters.php
+++ modules/newsletters/newsletters.php
@@ -3,12 +3,12 @@
 switch ($pg) {
     case "newsletters-enable":
         $object_newsletters = new newsletters();
-        $object_newsletters->setId(intval($_GET["i"]));
+        $object_newsletters->setId($id);
         $object_newsletters->enable();
         break;
     case "newsletters-disable":
         $object_newsletters = new newsletters();
-        $object_newsletters->setId(intval($_GET["i"]));
+        $object_newsletters->setId($id);
         $object_newsletters->disable();
         break;
 }
@@ -20,7 +20,7 @@ $newsletters_list = $object_newsletters->returnAllregistries();
 <h1 class="pageTitle">Newsletter Subscriptions</h1>
 <div class="category-list">
     <div class="button-area">
-        <a href="<?php print $configuration["path-bo"] ?>/0/newsletters-export/" class="grey" title="export"><i class="fa fa-download"></i></a>
+        <a href="<?= $configuration["path-bo"] ?>/0/newsletters-export/" class="grey" title="export"><i class="fa fa-download"></i></a>
     </div>
 
     <table class="db-list">
@@ -33,28 +33,35 @@ $newsletters_list = $object_newsletters->returnAllregistries();
         </tr>
         <?php
         foreach ($newsletters_list as $newsletter_entry) {
-            if ($newsletter_entry['active']) {
-                $active = '<img src="' . $configuration["path-bo"] . '/site-assets/images/icon_on.png" alt="on" />';
+            if ($newsletter_entry["active"]) {
+                $active = "<img src=\"{c2r-path-bo}/site-assets/images/icon_on.png\" alt=\"on\" />";
             } else {
-                $active = '<img src="' . $configuration["path-bo"] . '/site-assets/images/icon_off.png" alt="off" />';
+                $active = "<img src=\"{c2r-path-bo}/site-assets/images/icon_off.png\" alt=\"off\" />";
             }
 
-            print
-                    '<tr title="' . $newsletter_entry['date'] . '">' .
-                    '<td>' . $newsletter_entry['id'] . '</td>' .
-                    '<td>' . $newsletter_entry['email'] . '</td>' .
-                    '<td>' . $newsletter_entry['code'] . '</td>' .
-                    '<td>' . $active . '</td>' .
-                    '<td>' .
-                    '<a href="' . $configuration["path-bo"] . '/0/newsletters-enable/' . $newsletter_entry['id'] . '" class="green" title="enable"><i class="fa fa-check-circle-o"></i></a> ' .
-                    '<a href="' . $configuration["path-bo"] . '/0/newsletters-disable/' . $newsletter_entry['id'] . '" class="red" title="disable"><i class="fa fa-circle-o"></i></i></a>' .
-                    '</td>' .
-                    '</tr>';
-        }
+			print str_replace(
+				array(
+					"{c2r-date}",
+					"{c2r-id}",
+					"{c2r-email}",
+					"{c2r-code}",
+					"{c2r-active}",
+					"{c2r-path-bo}"
+				),
+				array(
+					$newsletter_entry['date'],
+					$newsletter_entry['id'],
+					$newsletter_entry['email'],
+					$newsletter_entry['code'],
+					$active,
+					$configuration["path-bo"]
+				),
+				file_get_contents($configuration["path-bo"]."/modules/newsletters/templates-e/line.html")
+			);
         ?>
     </table>
 
     <div class="button-area">
-        <a href="<?php print $configuration["path-bo"] ?>/0/newsletters-export/" class="grey" title="export"><i class="fa fa-download"></i></a>
+        <a href="<?= $configuration["path-bo"] ?>/0/newsletters-export/" class="grey" title="export"><i class="fa fa-download"></i></a>
     </div>
 </div>
