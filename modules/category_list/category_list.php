0994a9afcfa956d179e46ef8dfede39aff03a431
Carlos Santos <carlos@nexus-pt.eu>
2014-12-29 13:47:53 +0000

    new version

--- modules/category_list/category_list.php
+++ modules/category_list/category_list.php
@@ -5,8 +5,8 @@
 <h1 class="pageTitle">Category List</h1>
 <div class="category-list">
 	<div class="button-area">
-            <a href="<?php print $configuration["path-bo"] ?>/0/category-add/" class="green"><i class="fa fa-plus"></i></a>	
-        </div>
+		<a href="<?= $configuration["path-bo"] ?>/0/category-add/" class="green"><i class="fa fa-plus"></i></a>
+	</div>
 
 	<table class="db-list">
 		<tr>
@@ -17,23 +17,37 @@
 			<th>Sel.</th>
 		</tr>
 		<?php
-			foreach($category_list as $category){
-				if ($category['published']) {$published = '<img src="'.$configuration["path-bo"].'/site-assets/images/icon_on.png" alt="on" />';}
-					else {$published = '<img src="'.$configuration["path-bo"].'/site-assets/images/icon_off.png" alt="off" />';}
-				print
-				'<tr>'.
-				'<td>'.$category['id'].'</td>'.
-				'<td>'.$category['name_1'].'</td>'.
-				'<td>'.$category['category_type'].'</td>'.
-				'<td>'.$published.'</td>'.
-				'<td><a href="'.$configuration["path-bo"].'/0/category-edit/'.$category['id'].'" onclick="return confirm(\''.$language["template"]["are-you-sure"].'\')" class="orange"><i class="fa fa-pencil-square-o"></i></a> <a href="'.$configuration["path-bo"].'/0/category-del/'.$category['id'].'" onclick="return confirm(\''.$language["template"]["are-you-sure"].'\')" class="red"><i class="fa fa-trash"></i></a></td>'.
-				'</tr>';
+			foreach ($category_list as $category) {
+				if ($category['published']) {
+					$published = "<img src=\"{c2r-path-bo}/site-assets/images/icon_on.png\" alt=\"on\" />";
+				} else {
+					$published = "<img src=\"{c2r-path-bo}/site-assets/images/icon_off.png\" alt=\"off\" />";
+				}
 
+				print str_replace(
+					array(
+						"{c2r-id}",
+						"{c2r-name}",
+						"{c2r-type}",
+						"{c2r-published}",
+						"{c2r-path-bo}",
+						"{c2r-confirm}"
+					),
+					array(
+						$category["id"],
+						$category["name_1"],
+						$category["category_type"],
+						$published,
+						$configuration["path-bo"],
+						$language["template"]["are-you-sure"]
+					),
+					file_get_contents($configuration["path-bo"]."/modules/category_list/templates-e/line.html");
+				);
 			}
 		?>
 	</table>
 
 	<div class="button-area">
-            <a href="<?php print $configuration["path-bo"] ?>/0/category-add/" class="green"><i class="fa fa-plus"></i></a>
+		<a href="<?= $configuration["path-bo"] ?>/0/category-add/" class="green"><i class="fa fa-plus"></i></a>
 	</div>
 </div>
