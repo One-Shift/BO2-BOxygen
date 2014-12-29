278a99bd7c68e984bb383c2f1d044479067f9a9d
Carlos Santos <carlos@nexus-pt.eu>
2014-12-29 14:04:38 +0000

    new version

--- modules/home/home.php
+++ modules/home/home.php
@@ -1,2 +1,2 @@
-<h1 class="pageTitle"><?php print $language["home"]; ?></h1>
-<iframe src="http://nexus-pt.eu/version.php?version=<?php print $configuration["BO2-version"]; ?>" height="500px"></iframe>
+<h1 class="pageTitle"><?= $language["home"]; ?></h1>
+<iframe src="http://nexus-pt.eu/version.php?version=<?= $configuration["BO2-version"]; ?>" height="500px"></iframe>
