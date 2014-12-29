7b87bd06eb30119e1025cbf45a96ed730e7ba81b
Carlos Santos <carlos@nexus-pt.eu>
2014-12-29 10:15:03 +0000

    array new model

--- configuration.php
+++ configuration.php
@@ -1,58 +1,58 @@
 <?php
 
-$configuration = array(
-    // System
-    "minify" => true,
-    // Site info
-    "site-name" => " :: versão demo :: ",
-    "site-slogan" => "",
-    "site-owner" => "Carlos Santos",
-    "per-page" => 30,
-    "nr-links" => 4,
-    "path" => "Path-here ex.: /new or /old",
-    // back-end info
-    "path-bo" => "Path-above + bo folder ex.: /new/backoffice",
-    "BO2-version" => "1.1.0",
-    "BO2-subversion" => "d",
-    "BO2-name" => "BO2 / BOxygen",
-    "language" => "en",
-    "secret-key" => "GJTBpKregE9WgXc",
-    "category_sections" => array("products", "articles"),
-    // MySql configuration
-    "mysql-user" => "username",
-    "mysql-password" => "password",
-    "mysql-host" => "127.0.0.1",
-    "mysql-database" => "database",
-    "mysql-prefix" => "prefix",
-    // Cookie info
-    "cookie" => "cookie",
-    "cookie-time" => 320,
-    // SMTP configuration
-    "email" => "geral@nexus-pt.eu",
-    "mail-smtp" => "mail.yourdomain.here",
-    "mail-username" => "your@email.here",
-    "mail-password" => "yourpassword",
-    "mail-secure" => "connection-type eg.: plain",
-    // Languages
-    "lang_1_state" => true,
-    "lang_1_acronym" => "pt",
-    "lang_1_name" => "portuguese",
-    "lang_2_state" => true,
-    "lang_2_acronym" => "en",
-    "lang_2_name" => "english",
-    "lang_3_state" => true,
-    "lang_3_acronym" => "es",
-    "lang_3_name" => "spanish",
-    "lang_4_state" => true,
-    "lang_4_acronym" => "fr",
-    "lang_4_name" => "french",
-    "lang_5_state" => true,
-    "lang_5_acronym" => "5",
-    "lang_5_name" => "sample_5",
-    "lang_6_state" => true,
-    "lang_6_acronym" => "6",
-    "lang_6_name" => "sample_6",
-    "store" => true,
-    // Analytics
-    "analytics" => ""
-);
+$configuration = [
+	// System
+	"minify" => true,
+	// Site info
+	"site-name" => " :: versão demo :: ",
+	"site-slogan" => "",
+	"site-owner" => "Carlos Santos",
+	"per-page" => 30,
+	"nr-links" => 4,
+	"path" => "Path-here ex.: /new or /old",
+	// back-end info
+	"path-bo" => "Path-above + bo folder ex.: /new/backoffice",
+	"BO2-version" => "1.1.0",
+	"BO2-subversion" => "d",
+	"BO2-name" => "BO2 / BOxygen",
+	"language" => "en",
+	"secret-key" => "GJTBpKregE9WgXc",
+	"category_sections" => array("products", "articles"),
+	// MySql configuration
+	"mysql-user" => "username",
+	"mysql-password" => "password",
+	"mysql-host" => "127.0.0.1",
+	"mysql-database" => "database",
+	"mysql-prefix" => "prefix",
+	// Cookie info
+	"cookie" => "cookie",
+	"cookie-time" => 320,
+	// SMTP configuration
+	"email" => "geral@nexus-pt.eu",
+	"mail-smtp" => "mail.yourdomain.here",
+	"mail-username" => "your@email.here",
+	"mail-password" => "yourpassword",
+	"mail-secure" => "connection-type eg.: plain",
+	// Languages
+	"lang_1_state" => true,
+	"lang_1_acronym" => "pt",
+	"lang_1_name" => "portuguese",
+	"lang_2_state" => true,
+	"lang_2_acronym" => "en",
+	"lang_2_name" => "english",
+	"lang_3_state" => true,
+	"lang_3_acronym" => "es",
+	"lang_3_name" => "spanish",
+	"lang_4_state" => true,
+	"lang_4_acronym" => "fr",
+	"lang_4_name" => "french",
+	"lang_5_state" => true,
+	"lang_5_acronym" => "5",
+	"lang_5_name" => "sample_5",
+	"lang_6_state" => true,
+	"lang_6_acronym" => "6",
+	"lang_6_name" => "sample_6",
+	"store" => true,
+	// Analytics
+	"analytics" => ""
+];
