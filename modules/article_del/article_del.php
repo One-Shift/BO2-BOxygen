87d460c27aa30914dfadc06ed622ae8e2f0652e3
Carlos Santos <carlos@nexus-pt.eu>
2014-12-29 12:20:06 +0000

    new version

--- modules/article_del/article_del.php
+++ modules/article_del/article_del.php
@@ -1,9 +1,14 @@
 <h1 class="pageTitle">Article Del</h1>
 <?php
-	if (isset($_GET['i']) && !empty($_GET['i'])) {
+	if ($id !== null) {
 		$object_article = new article();
-		$object_article->setId($_REQUEST['i']);
-		if($object_article->delete()) print $language["actions"]["success"]; else print $language["actions"]["failure"];
+		$object_article->setId($id);
+
+		if ($object_article->delete()) {
+			print $language["actions"]["success"];
+		} else {
+			print $language["actions"]["failure"];
+		}
 	}else{
 		print $language["actions"]["error"];
 	}
