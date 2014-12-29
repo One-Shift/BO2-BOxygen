f490f96545e13304b32bbeed1c557b009e7caa4e
Carlos Santos <carlos@nexus-pt.eu>
2014-12-29 14:03:32 +0000

    new version

--- includes.php
+++ includes.php
@@ -1,83 +1,88 @@
 <?php
 
 if ($account["login"]) { // verificar se está autenticado
-    switch ($pg) {
-        case "home":
-            include "./modules/home/home.php";
-            break;
-        // -- Article
-        case "article-add":
-            include "./modules/article_add/article_add.php";
-            break;
-        case "article-edit":
-            include "./modules/article_edit/article_edit.php";
-            break;
-        case "article-del":
-            include "./modules/article_del/article_del.php";
-            break;
-        case "article-list":
-            include "./modules/article_list/article_list.php";
-            break;
-        // -- Products
-        case "product-add":
-            include "./modules/product_add/product_add.php";
-            break;
-        case "product-edit":
-            include "./modules/product_edit/product_edit.php";
-            break;
-        case "product-del":
-            include "./modules/product_del/product_del.php";
-            break;
-        case "product-list":
-            include "./modules/product_list/product_list.php";
-            break;
-        case "orders": include "./modules/orders/orders.php";
-            break;
-        // -- Categories
-        case "category-add":
-            include "./modules/category_add/category_add.php";
-            break;
-        case "category-edit":
-            include "./modules/category_edit/category_edit.php";
-            break;
-        case "category-del":
-            include "./modules/category_del/category_del.php";
-            break;
-        case "category-list":
-            include "./modules/category_list/category_list.php";
-            break;
-        // -- User Info
-        case "user-add":
-            include "./modules/user_add/user_add.php";
-            break;
-        case "user-edit":
-            include "./modules/user_edit/user_edit.php";
-            break;
-        case "user-del":
-            include "./modules/user_del/user_del.php";
-            break;
-        case "user-list":
-            include "./modules/user_list/user_list.php";
-            break;
-        // -- Newsletters
-        case "newsletters":
-            include "./modules/newsletters/newsletters.php";
-            break;
-        case "newsletters-enable":
-            include "./modules/newsletters/newsletters.php";
-            break;
-        case "newsletters-disable":
-            include "./modules/newsletters/newsletters.php";
-            break;
-        // -- Session
-        case "logout":
-            include "./modules/logout/logout.php";
-            break;
+	switch ($pg) {
+		case "home":
+			include "./modules/home/home.php";
+			break;
+		// -- Article
+		case "article-add":
+			include "./modules/article_add/article_add.php";
+			break;
+		case "article-edit":
+			include "./modules/article_edit/article_edit.php";
+			break;
+		case "article-del":
+			include "./modules/article_del/article_del.php";
+			break;
+		case "article-list":
+			include "./modules/article_list/article_list.php";
+			break;
+		// -- Products
+		case "product-add":
+			include "./modules/product_add/product_add.php";
+			break;
+		case "product-edit":
+			include "./modules/product_edit/product_edit.php";
+			break;
+		case "product-del":
+			include "./modules/product_del/product_del.php";
+			break;
+		case "product-list":
+			include "./modules/product_list/product_list.php";
+			break;
+		case "orders": include "./modules/orders/orders.php";
+			break;
+		// -- Categories
+		case "category-add":
+			include "./modules/category_add/category_add.php";
+			break;
+		case "category-edit":
+			include "./modules/category_edit/category_edit.php";
+			break;
+		case "category-del":
+			include "./modules/category_del/category_del.php";
+			break;
+		case "category-list":
+			include "./modules/category_list/category_list.php";
+			break;
+		// -- User Info
+		case "user-add":
+			include "./modules/user_add/user_add.php";
+			break;
+		case "user-edit":
+			include "./modules/user_edit/user_edit.php";
+			break;
+		case "user-del":
+			include "./modules/user_del/user_del.php";
+			break;
+		case "user-list":
+			include "./modules/user_list/user_list.php";
+			break;
+		// -- Newsletters
+		case "newsletters":
+			include "./modules/newsletters/newsletters.php";
+			break;
+		case "newsletters-enable":
+			include "./modules/newsletters/newsletters.php";
+			break;
+		case "newsletters-disable":
+			include "./modules/newsletters/newsletters.php";
+			break;
 
-        default:
-            include "./modules/not_found/not-found.php";
-            break;
-    }
+		// -- files
+		case "controller-files":
+			include "./modules/controller-files/controller_files.php";
+			break;
+		// -- Session
+		case "logout":
+			include "./modules/logout/logout.php";
+			break;
+
+		default:
+			include "./modules/not_found/not-found.php";
+			break;
+	}
 } else {
-    printf("<script>goTo('%s');</script>", $configuration["path-bo"]);
+	printf("<script>goTo('%s');</script>", $configuration["path-bo"]);
 }
