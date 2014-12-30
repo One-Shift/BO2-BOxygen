e2287a48d2a713584107e0d01b1b6a5bc6db9d77
Carlos Santos <carlos@nexus-pt.eu>
2014-12-30 16:47:38 +0000

    add subversion verification

--- modules/home/home.php
+++ modules/home/home.php
@@ -1,2 +1,2 @@
 <h1 class="pageTitle"><?= $language["home"]["title"]; ?></h1>
-<iframe src="http://nexus-pt.eu/version.php?version=<?= $configuration["BO2-version"]; ?>" height="500px"></iframe>
+<iframe src="http://nexus-pt.eu/version.php?v=<?= $configuration["BO2-version"]; ?>&sv=<?= $configuration["BO2-subversion"]; ?>" height="500px"></iframe>
