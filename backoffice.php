0bafe132147183b277feee724088f68b0eaf5f30
Carlos Santos <carlos@nexus-pt.eu>
2014-12-30 00:12:04 +0000

    add analytics to BO2

--- backoffice.php
+++ backoffice.php
@@ -101,6 +101,16 @@ if ($pg == "logout") {
 		<script type="text/javascript" src="<?= $configuration["path-bo"] ?>/site-assets/js/jquery.js"></script>
 		<script type="text/javascript" src="<?= $configuration["path-bo"] ?>/site-assets/js/nicEdit.js"></script>
 		<script type="text/javascript" src="<?= $configuration["path-bo"] ?>/site-assets/js/script.js"></script>
+        <script>
+          (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+          (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+          m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+          })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+          ga('create', 'UA-36909778-11', 'auto');
+          ga('send', 'pageview');
+
+        </script>
 	</head>
 	<body>
 		<iframe class="ads" src="http://www.nexus-pt.eu/ads.php"></iframe>
