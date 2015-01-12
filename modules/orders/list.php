9bc100888aeafb3737e9c8fb4855694479fd58de
Carlos Santos <carlos@nexus-pt.eu>
2015-01-12 12:55:37 +0000

    new version

--- modules/orders/list.php
+++ modules/orders/list.php
@@ -15,36 +15,44 @@
 		<th>Sel.</th>
 	</tr>
 	<?php
-	foreach ($orders_list as $order) {
-		if ($order["status"]) {
-			$published = "<img src=\"{c2r-path-bo}/site-assets/images/icon_on.png\" alt=\"on\" />";
-		} else {
-			$published = "<img src=\"{c2r-path-bo}/site-assets/images/icon_off.png\" alt=\"off\" />";
+	if (count($orders_list) != 0) {
+		foreach ($orders_list as $order) {
+			if ($order["status"]) {
+				$published = "<img src=\"{c2r-path-bo}/site-assets/images/icon_on.png\" alt=\"on\" />";
+			} else {
+				$published = "<img src=\"{c2r-path-bo}/site-assets/images/icon_off.png\" alt=\"off\" />";
+			}
+
+			$object_user = new user();
+			$object_user->setId($order["user_id"]);
+			$user = $object_user->returnOneUser();
+
+			print str_replace(
+				array(
+					"{c2r-id}",
+					"{c2r-user-name}",
+					"{c2r-date-update}",
+					"{c2r-published}",
+					"{c2r-path-bo}",
+					"{c2r-confirm}"
+				),
+				array(
+					$order["id"],
+					$user["name"],
+					$order["date_update"],
+					$published,
+					$configuration["path-bo"],
+					$language["template"]["areyousure"]
+				),
+				file_get_contents("./modules/orders/templates-e/line.html")
+			);
 		}
-		
-		$object_user = new user();
-		$object_user->setId($order["user_id"]);
-		$user = $object_user->returnOneUser();
-		
+	} else {
 		print str_replace(
-			array(
-				"{c2r-id}",
-				"{c2r-user-name}",
-				"{c2r-date-update}",
-				"{c2r-published}",
-				"{c2r-path-bo}",
-				"{c2r-confirm}"
-			),
-			array(
-				$order["id"],
-				$user["name"],
-				$order["date_update"],
-				$published,
-				$configuration["path-bo"],
-				$language["template"]["areyousure"]
-			),
-			file_get_contents("./modules/orders/templates-e/line.html")
-		);
+				"{c2r-noresults}", 
+				$language["template"]["noresults"],
+				file_get_contents("./modules/orders/templates-e/answer-line-noresults.html")
+			);
 	}
 	?>
 </table>
