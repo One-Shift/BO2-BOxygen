0586220d2e934b0552ac579a347b5bda56a9e482
Carlos Santos <c42759@users.noreply.github.com>
2016-11-22 14:48:55 +0000

    new connect option

--- connect.php
+++ connect.php
@@ -1,10 +1,12 @@
 <?php
 
-$mysqli = mysqli_connect($configuration["mysql-host"], $configuration["mysql-user"], $configuration["mysql-password"], $configuration["mysql-database"]);
+if ($configuration["mysql-connect"]) {
+    $mysqli = mysqli_connect($configuration["mysql-host"], $configuration["mysql-user"], $configuration["mysql-password"], $configuration["mysql-database"]);
 
-if (mysqli_connect_errno()) {
-    printf("Connect failed: %s\n", mysqli_connect_error());
-    exit();
+    if (mysqli_connect_errno()) {
+        printf("Connect failed: %s\n", mysqli_connect_error());
+        exit();
+    }
+
+    $mysqli->set_charset("utf8");
 }
-
-$mysqli->set_charset("utf8");
